037101fdd35232ef41999c2e3047ca148ab9807b
Ramon Cândido <ramoncandido64@gmail.com>
2025-04-06 04:18:14 -0300

    Adicionada componente navbar ao arquivo principal

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import Navbar from './components/Navbar.vue';
 
 const app = createApp(App);
+app.component('Navbar', Navbar);
 app.use(router);
 app.mount('#app');
