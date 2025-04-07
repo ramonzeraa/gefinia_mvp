22502cec6d08b30a993684acfa233ca5bf65d30c
Ramon Cândido <ramoncandido64@gmail.com>
2025-04-07 20:34:03 -0300

    Inicializado login via google + bibliotecas necessarias

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import Navbar from './components/Navbar.vue';
+import vue3GoogleLogin from 'vue3-google-login';
 
 const app = createApp(App);
 app.component('Navbar', Navbar);
 app.use(router);
+app.use(vue3GoogleLogin, {
+  clientId: GOOGLE_CLIENT_ID,
+});
 app.mount('#app');
